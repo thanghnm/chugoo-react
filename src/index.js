c69bedc1fd0dac421cdd24ab8d6fae2b81db4066
Veloc1tyE <w.jeremijenko@gmail.com>
2019-07-29 20:50:25 +1000

    lacing backend together

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from './rootReducer'
+
+const store = createStore(rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
